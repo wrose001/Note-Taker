38a49acc9d75676986d6180d0e08d725837e1b89
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-03-04 19:02:33 -0600

    Modified code to assign unique id to notes in JSON. Also, added data attribute to index.js file and changed the code slightly to be able to delete note when I select the trash icon. Code is working properly. The next step will be to update my README.md file.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -115,7 +115,7 @@ var renderNoteList = function(notes) {
     var $li = $("<li class='list-group-item'>").data(note);
     var $span = $("<span>").text(note.title);
     var $delBtn = $(
-      "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      "<i data-id='"+note.id+"' class='fas fa-trash-alt float-right text-danger delete-note'>"
     );
 
     $li.append($span, $delBtn);
